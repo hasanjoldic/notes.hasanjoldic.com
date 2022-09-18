500bfd77a64f647dcd94b0e3b0ab5bd621c7ccad
Hasan Joldic <hasan.joldic@enki.ba>
2022-09-18 07:54:38 +0200

    refactor: update deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,23 @@
 import type { AppContext, AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import styled from "@mui/material/styles/styled";
-import type { PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 
 import { ThemeProvider } from "@hasan.joldic/theme";
 import { Page } from "@hasan.joldic/components";
 
-import { useRouter } from "next/router";
-
-import "../styles/globals.css";
-
 const Content = styled("div")(({ theme }) => ({
   maxWidth: theme.breakpoints.values.lg,
 }));
 
 interface Props extends AppProps {
-  paletteMode: PaletteMode;
+  cookie: string | undefined;
 }
 
-const App = ({ Component, pageProps, paletteMode }: Props) => {
+const App = ({ Component, pageProps, cookie }: Props) => {
   const router = useRouter();
 
   const handleNavigate = (path: string) => {
@@ -29,7 +25,7 @@ const App = ({ Component, pageProps, paletteMode }: Props) => {
   };
 
   return (
-    <ThemeProvider paletteMode={paletteMode}>
+    <ThemeProvider cookie={cookie}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <title>Hasan Joldic</title>
@@ -48,17 +44,12 @@ const App = ({ Component, pageProps, paletteMode }: Props) => {
   );
 };
 
-App.getInitialProps = async (context: AppContext) => {
-  const paletteMode = getCookie("paletteMode", context.ctx.req?.headers.cookie);
+App.getInitialProps = async (
+  context: AppContext
+): Promise<Omit<Props, keyof AppProps>> => {
+  const cookie = context.ctx.req?.headers.cookie;
 
-  return { paletteMode } as unknown as Props;
+  return { cookie };
 };
-
-function getCookie(name: string, cookie?: string) {
-  return cookie
-    ?.split(";")
-    ?.find((row) => row.includes(name))
-    ?.split("=")[1];
-}
 
 export default App;
