71d3405015280087248ace27f91f667e1eefaa5b
Hasan Joldic <office@hasanjoldic.com>
2022-09-19 17:23:02 +0000

    feat: update packages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,3 @@
-import {
-  default as NextDocument,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from "next/document";
+import { Document } from "@hasanjoldic/nextjs";
 
-export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="shortcut icon"
-            href="https://static.hasanjoldic.com/hasanjoldic.com/favicon.ico"
-          />
-          <meta name="description" content="Hasan Joldic | Notes" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default Document;
